91cf4d8a3971c4ed30a78772ee8af3adfebae32c
kuroda-ke <kuroda-ke@solidcom.co.jp>
2022-07-10 15:47:01 +0900

    test

--- src/components/pages/Good.tsx
+++ src/components/pages/Good.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainTemplate from "../templete/Maintemplete";
 
 const Good: React.FC = (props) => {
-  return <MainTemplate title="good" content={<h1>よい</h1>} />;
+  return <MainTemplate title="good" content={<h1>いいね</h1>} />;
 };
 
 export default Good;
